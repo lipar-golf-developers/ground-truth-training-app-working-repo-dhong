5441827db7f45133c6cfbec5e31f5fe0060efba2
Hanson Tram <htram.dev@gmail.com>
2024-03-15 12:26:56 -0700

    Add return button to record screen

--- Screens/Record.js
+++ Screens/Record.js
@@ -5,10 +5,10 @@ import { globalStyles } from "../shared/GlobalStyles";
 import { Camera } from "expo-camera";
 import { Video } from "expo-av";
 import { safeAreaView } from "react-native-safe-area-context";
-import { shareAsync } from 'expo-sharing';
-import * as MediaLibrary from 'expo-media-library'
+import { shareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
 
-export default function Record() {
+export default function Record({ navigation }) {
   let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [hasMicrophonePermission, setHasMicrophonePermission] = useState();
@@ -16,78 +16,91 @@ export default function Record() {
   const [isRecording, setIsRecording] = useState(false);
   const [video, setVideo] = useState();
 
- 
-  useEffect(() =>{
-    (async() => {
+  useEffect(() => {
+    (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
 
       setHasCameraPermission(cameraPermission.status === "granted");
       setHasMicrophonePermission(microphonePermission.status === "granted");
       setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-
     })();
   }, []);
 
-if (hasCameraPermission === undefined || hasMicrophonePermission === undefined){
-  return <Text>Requesting Permissions...</Text>
-}  else if (!hasCameraPermission) {
-  return <Text>Camera permissions not granted!</Text>
-}
-
-let recordVideo = () => {
-  setIsRecording(true);
-  let options = {
-    quality: "1080p",
-    mute: false
+  if (
+    hasCameraPermission === undefined ||
+    hasMicrophonePermission === undefined
+  ) {
+    return <Text>Requesting Permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return <Text>Camera permissions not granted!</Text>;
   }
-  cameraRef.current.recordAsync(options).then((recordedVideo) => {
-    setVideo(recordedVideo);
-    setIsRecording(false);
-  });
-}
 
-let stopRecording = () => {
-  setIsRecording(false);
-  cameraRef.current.stopRecording();
-}
-
-if (video) {
-  let shareVideo = () => {
-    shareAsync(video.uri).then(() => {
-      setVideo(undifined);
+  let recordVideo = () => {
+    setIsRecording(true);
+    let options = {
+      quality: "1080p",
+      mute: false,
+    };
+    cameraRef.current.recordAsync(options).then((recordedVideo) => {
+      setVideo(recordedVideo);
+      setIsRecording(false);
     });
   };
 
-let saveVideo = () => {
-  MediaLibrary.saveToLibraryAsync(video.uri).then(() => {
-    setVideo
-  });
-};
+  let stopRecording = () => {
+    setIsRecording(false);
+    cameraRef.current.stopRecording();
+  };
 
-  return (
-  <SafeAreaView style = {styles.container}>
-    <Video
-      style={styles.video}
-      source={{uri: video.uri}}
-      useNativeControls
-      resizeMode = 'contain'
-      isLooping
-    />
-    <Button title="Share" onPress={shareVideo}/>
-    {hasMediaLibraryPermission ? <Button title="Save" onPress={saveVideo} />: undefined}
-    <Button title="Delete" onPress={() => setVideo(undefined)}/>
-  </SafeAreaView>
-  );
-}
+  if (video) {
+    let shareVideo = () => {
+      shareAsync(video.uri).then(() => {
+        setVideo(undifined);
+      });
+    };
 
+    let saveVideo = () => {
+      MediaLibrary.saveToLibraryAsync(video.uri).then(() => {
+        setVideo;
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Video
+          style={styles.video}
+          source={{ uri: video.uri }}
+          useNativeControls
+          resizeMode="contain"
+          isLooping
+        />
+        <Button title="Share" onPress={shareVideo} />
+        {hasMediaLibraryPermission ? (
+          <Button title="Save" onPress={saveVideo} />
+        ) : undefined}
+        <Button title="Delete" onPress={() => setVideo(undefined)} />
+      </SafeAreaView>
+    );
+  }
+  const returnButtonHandler = () => {
+    navigation.navigate("SessionStart");
+  };
   return (
-      <Camera style={styles.container} ref = {cameraRef}>
-        <View style={styles.buttonContainer}>
-          <Button title={isRecording ? "Stop Recording" : "Record Video"} onPress={isRecording ? stopRecording : recordVideo} />
-        </View>
-      </Camera>
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={isRecording ? "Stop Recording" : "Record Video"}
+          onPress={isRecording ? stopRecording : recordVideo}
+        />
+      </View>
+      <View style={styles.returnButtonContainer}>
+        <Button title="return" onPress={returnButtonHandler} />
+      </View>
+    </Camera>
   );
 }
 
@@ -102,10 +115,18 @@ const styles = StyleSheet.create({
     position: "absolute",
     alignSelf: "center",
     bottom: 50,
-    borderRadius: 10
+    borderRadius: 10,
+  },
+  returnButtonContainer: {
+    backgroundColor: "#151720",
+    position: "absolute",
+    alignSelf: "center",
+    top: 50,
+    left: 20,
+    borderRadius: 10,
   },
   video: {
-    flex:1,
-    alignSelf: "stretch"
-  }
+    flex: 1,
+    alignSelf: "stretch",
+  },
 });
